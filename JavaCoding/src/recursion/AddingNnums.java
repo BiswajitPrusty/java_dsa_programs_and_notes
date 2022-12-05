fbcc4f6fa6d2f7f7ba2db16bd695d695e7357d7d
BiswajitPrusty <biswajit.prusty@atyeti.com>
2022-12-05 14:45:19 +0530

    factorial and Fibonacci using recursion

--- JavaCoding/src/recursion/AddingNnums.java
+++ JavaCoding/src/recursion/AddingNnums.java
@@ -2,17 +2,14 @@ package recursion;
 
 public class AddingNnums {
 
-	public static int add(int n) {
+    public static int add(int n) {
+        if (n == 1)
+            return n;
+        return n + add(n - 1);
+    }
 
-		if (n == 1) {
-			return n;
-		}
-
-		return n + add(n - 1);
-	}
-	
-	public static void main(String[] args) {
-		int res =add(5);
-		System.out.println(res);
-	}
+    public static void main(String[] args) {
+        int res = add(5);
+        System.out.println(res);
+    }
 }
