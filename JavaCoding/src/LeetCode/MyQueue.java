d8e79ef7da798ecc694ce76d4c1c9b9f1c4759fc
BiswajitPrusty <biswajitprusty165@gmail.com>
2022-12-17 07:21:00 +0530

    Queue implementation using Stacks

--- JavaCoding/src/LeetCode/MyQueue.java
+++ JavaCoding/src/LeetCode/MyQueue.java
@@ -1,6 +1,11 @@
 package LeetCode;
 
+import java.util.Stack;
+
 class MyQueue {
+
+    public Stack<Integer> stack1 = new Stack<>();
+    public Stack<Integer> stack2 = new Stack<>();
 
     public MyQueue() {
 
@@ -8,21 +13,32 @@ class MyQueue {
 
     public void push(int x) {
 
+        stack1.push(x);
 
     }
 
     public int pop() {
 
-        return -1;
+        if (stack2.isEmpty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
     }
 
     public int peek() {
 
-        return -1;
+        if (stack2.isEmpty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
     }
 
     public boolean empty() {
 
-        return false;
+        return stack1.isEmpty() && stack2.empty();
     }
 }
