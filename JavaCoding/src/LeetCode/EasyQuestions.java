eb96723e3966ed6f9f7d569104e026e811144c63
BiswajitPrusty <biswajitprusty165@gmail.com>
2022-12-19 21:33:58 +0530

    2 easy questions, merge and quick sort

--- JavaCoding/src/LeetCode/EasyQuestions.java
+++ JavaCoding/src/LeetCode/EasyQuestions.java
@@ -1,6 +1,8 @@
 package LeetCode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class EasyQuestions {
 
@@ -9,6 +11,53 @@ public class EasyQuestions {
 //        e.romanToInt("MCMXCIV");
 //        e.reverseString(new char[]{'h', 'e', 'l'});
 //        e.singleNumber(new int[]{'h', 'h', 'l'});
+        System.out.println(e.fizzBuzz(3));
+    }
+
+    /**
+     * Given an integer n, return a string array answer (1-indexed) where:
+     * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+     * answer[i] == "Fizz" if i is divisible by 3.
+     * answer[i] == "Buzz" if i is divisible by 5.
+     * answer[i] == i (as a string) if none of the above conditions are true.
+     * Example 1:
+     * Input: n = 3
+     * Output: ["1","2","Fizz"]
+     */
+    public List<String> fizzBuzz(int n) {
+
+        List<String> answer = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                answer.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                answer.add("Fizz");
+            } else if (i % 5 == 0) {
+                answer.add("Buzz");
+            } else {
+                answer.add(i + "");
+            }
+        }
+        return answer;
+    }
+
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return insertToBst(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode insertToBst(int[] nums, int l, int r) {
+
+        if (l > r) {
+            return null;
+        }
+        //choose the middle element and
+        int mid = l + (r - l) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = insertToBst(nums, l, mid - 1);
+        node.right = insertToBst(nums, mid + 1, r);
+        return node;
+
     }
 
     public int singleNumber(int[] nums) {
--- JavaCoding/src/Practice/AllAlogs.java
+++ JavaCoding/src/Practice/AllAlogs.java
@@ -2,11 +2,15 @@ package Practice;
 
 public class AllAlogs {
 
+    int length;
+    int temp[];
+    int array[];
+
     public static void main(String[] args) {
 
         AllAlogs algo = new AllAlogs();
         int a[] = {9, 1, 4, 3, 2, 6, 5, 8, 7};
-        algo.insertionSort(a);
+        algo.mergeSort(a);
 
         printArray(a);
 
@@ -17,6 +21,70 @@ public class AllAlogs {
             System.out.print(a[i] + ", ");
         }
         System.out.println();
+    }
+
+    public void quickSort(int a[], int low, int high) {
+
+        int p = divideQuickSort(a, low, high);
+        if (low < p - 1) {
+            quickSort(a, low, p - 1);
+        }
+        if (high > p) {
+            quickSort(a, p, high);
+        }
+    }
+
+    public int divideQuickSort(int a[], int low, int high) {
+        int pi = a[(low + high) / 2];
+        while (low <= high) {
+            while (a[low] < pi) {
+                low++;
+            }
+            while (a[high] > pi) {
+                high--;
+            }
+            if (low <= high) {
+                int temp = a[low];
+                a[low] = a[high];
+                a[high] = temp;
+                low++;
+                high--;
+            }
+        }
+        return low;
+    }
+
+    public void mergeSort(int a[]) {
+        this.array = a;
+        this.length = a.length;
+        temp = new int[this.length];
+        divideMergeSort(0, this.length - 1);
+    }
+
+    private void divideMergeSort(int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            divideMergeSort(low, mid);
+            divideMergeSort(mid + 1, high);
+            mergeSortLogic(low, mid, high);
+        }
+    }
+
+    private void mergeSortLogic(int low, int mid, int high) {
+        for (int i = low; i <= high; i++) {
+            temp[i] = array[i];
+        }
+        int i = low, j = mid + 1, k = low;
+        while (i <= mid && j <= high) {
+            if (temp[i] <= temp[j]) {
+                array[k++] = temp[i++];
+            } else {
+                array[k++] = temp[j++];
+            }
+        }
+        while (i <= mid) {
+            array[k++] = temp[i++];
+        }
     }
 
     public void bubbleSort(int a[]) {
@@ -63,15 +131,5 @@ public class AllAlogs {
             a[j] = temp;
         }
     }
-
-    public void mergeSort() {
-
-
-    }
-
-    public void quickSort() {
-
-    }
-
 
 }
