16e156cd6d32ceea29e6de7f1077738ed72a1057
BiswajitPrusty <biswajitprusty165@gmail.com>
2023-01-11 11:21:46 +0530

    easy questions

---
+++ JavaCoding/src/easyQuestions/maths/AddBinary67.java
@@ -0,0 +1,14 @@
+package easyQuestions.maths;
+
+public class AddBinary67 {
+
+    public static void main(String[] args) {
+        addBinary("10", "10");
+    }
+
+    public static String addBinary(String a, String b) {
+
+        return null;
+    }
+
+}
