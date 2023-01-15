5e226f258023a56cfbda5f11eb615f4571a6535a
BiswajitPrusty <biswajitprusty165@gmail.com>
2023-01-15 19:04:14 +0530

    easy questions

---
+++ JavaCoding/src/easyQuestions/strings/FinalValueOfVariable2011.java
@@ -0,0 +1,32 @@
+package easyQuestions.strings;
+
+public class FinalValueOfVariable2011 {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+
+        for (int i = 0; i < operations.length; i++) {
+
+            switch (operations[i]) {
+                case "--X": {
+                    --x;
+                    break;
+                }
+                case "X--": {
+                    x--;
+                    break;
+                }
+                case "X++": {
+                    x++;
+                    break;
+                }
+                case "++X": {
+                    ++x;
+                    break;
+                }
+            }
+        }
+
+        return x;
+    }
+
+}
