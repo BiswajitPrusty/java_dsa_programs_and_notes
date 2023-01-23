c0a4f00d32583a4b90208df435c47612140c6100
BiswajitPrusty <biswajitprusty165@gmail.com>
2023-01-23 14:39:27 +0530

    computing power

---
+++ JavaCoding/src/easyQuestions/maths/ComputingPower.java
@@ -0,0 +1,17 @@
+package easyQuestions.maths;
+
+public class ComputingPower {
+
+    public static void main(String[] args) {
+        power(2,3);
+    }
+
+    //naive approach
+    public static int power(int x, int y) {
+        int result = 1;
+        for (int i = 0; i < y; i++) {
+            result *= x;
+        }
+      return result;
+    }
+}
--- JavaCoding/src/easyQuestions/maths/SieveOfEratosthenes.java
+++ JavaCoding/src/easyQuestions/maths/SieveOfEratosthenes.java
@@ -14,7 +14,7 @@ public class SieveOfEratosthenes {
 
         for (int i = 2; i * i <= n; i++) {
             if (isPrime[i]) {
-                for (int j = 2 * i; j <= n; j += i) {
+                for (int j = i * i; j <= n; j += i) {
                     isPrime[j] = false;
                 }
             }
