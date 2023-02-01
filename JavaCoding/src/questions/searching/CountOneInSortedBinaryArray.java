2492b92147df7465443c1fd7010d91a8234b7ab1
BiswajitPrusty <biswajitprusty165@gmail.com>
2023-02-01 20:42:31 +0530

    question on binary search algo

--- JavaCoding/src/questions/searching/CountOneInSortedBinaryArray.java
+++ JavaCoding/src/questions/searching/CountOneInSortedBinaryArray.java
@@ -4,8 +4,26 @@ public class CountOneInSortedBinaryArray {
 
     public static void main(String[] args) {
         System.out.println(countOnes(new int[]{0, 0, 0, 0, 1, 1, 1, 1}, 1));
+        System.out.println(countOnesOptimized(new int[]{0, 0, 0, 0, 1, 1, 1, 1}));
     }
 
+    public static int countOnesOptimized(int arr[]) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == 0) {
+                low = mid + 1;
+            } else {
+                if (mid == 0 || arr[mid - 1] == 0)
+                    return arr.length - mid;
+                else
+                    high = mid - 1;
+            }
+        }
+        return 0;
+    }
+
+    //  my approach
     public static int countOnes(int[] arr, int target) {
         int low = 0, high = arr.length - 1;
         int count = -1;
