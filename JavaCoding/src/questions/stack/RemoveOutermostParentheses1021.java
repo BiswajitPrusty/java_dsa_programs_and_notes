f88daeabc2655b15a7e431a4089f5b9ead282579
BiswajitPrusty <biswajitprusty165@gmail.com>
2023-02-13 12:37:42 +0530

    question on stack

---
+++ JavaCoding/src/questions/stack/RemoveOutermostParentheses1021.java
@@ -0,0 +1,18 @@
+package questions.stack;
+
+import java.util.Stack;
+
+public class RemoveOutermostParentheses1021 {
+
+    public String removeOuterParentheses(String s) {
+
+        String ans = null;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            
+            
+        }
+        return ans;
+    }
+}
